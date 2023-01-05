785d7a957c8ca5a75a8e29d4e9ff21f7f49d3ef3
Ruii Ying Lee <ruiiying.l@gmail.com>
2023-01-05 17:07:12 +0800

    latest update 1707

--- Responder - 20221205 - MARS with routing - NoSSL/app/src/main/java/com/example/st/arcgiscss/activites/SelectIPActivity.java
+++ Responder - 20221205 - MARS with routing - NoSSL/app/src/main/java/com/example/st/arcgiscss/activites/SelectIPActivity.java
@@ -94,7 +94,7 @@ public class SelectIPActivity extends Activity implements View.OnClickListener {
 
         //ZN - 20220710 put in default string
         if (ip == null || ip.equalsIgnoreCase( ""));
-              ip = "172.20.10.10";
+              ip = "192.168.43.152";
 
         et_ip.setText(ip);
 
